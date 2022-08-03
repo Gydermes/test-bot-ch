11e1552487c8ef788bcf6612bf8fa8dd430cb3fd
Gydermes <110401357+Gydermes@users.noreply.github.com>
2022-08-03 15:46:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def get_message():
 @app.route('/')
 def main():
     bot.remove_webhook()
-    bot.set_webhook(url='https://dashboard.heroku.com/apps/test-bot-che' + TOKEN)
+    bot.set_webhook(url='https://test-bot-che.herokuapp.com/' + TOKEN)
     return 'Python Telegram Bot', 200
 
 

b3b829e79e5e077a6d5209446c75356ab954b6de
Gydermes <110401357+Gydermes@users.noreply.github.com>
2022-08-03 12:31:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def get_message():
 @app.route('/')
 def main():
     bot.remove_webhook()
-    bot.set_webhook(url='https://test-bot-june.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://test-bot-che.herokuapp.com/' + TOKEN)
     return 'Python Telegram Bot', 200
 
 
